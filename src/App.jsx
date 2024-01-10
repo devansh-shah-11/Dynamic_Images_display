a30f20c8f7d8b6cdddd79654a6f1947f037817b8
Devansh Shah <devanshnshah11@gmail.com>
2024-01-10 17:39:27 +0530

    Multiple_Image_Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ function App() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch('https://dog.ceo/api/breeds/image/random')
+        fetch('https://dog.ceo/api/breeds/image/random/2')
         .then(response => response.json())
         .then(json => setData(json))
         .catch(error => console.error(error));
     }, []);
 
     console.log(data);
-    
+
     return (
         <div>
-            <img src={data && data.message} alt="dog" />
+            <h1>Dogs</h1>
+            {data && data.message.map((url, index) => (
+                <img key={index} src={url} alt="dog" />
+            ))}
         </div>
     );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -14,6 +14,7 @@ function Counter() {
       <span>{counter}</span>
       {'\t'}
       <button onClick={SubtractCounter}>-</button>
+      <br />
     </div>
   )
 }
