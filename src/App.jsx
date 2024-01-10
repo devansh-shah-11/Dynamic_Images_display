fcc31d9af9998d690bfa65375d22beeba7f0e5c6
Devansh Shah <devanshnshah11@gmail.com>
2024-01-10 17:21:44 +0530

    Fetching_Random_Images_From_API

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
-function Counter() {
+function App() {
+    const [data, setData] = useState(null);
 
-  const [counter, setCounter] = useState(0)
-  const AddCounter = () => setCounter(counter + 1)
-  const SubtractCounter = () => setCounter(counter - 1)
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(json => setData(json))
+        .catch(error => console.error(error));
+    }, []);
 
-  console.log('rendering...', counter)
-  return (
-    <div>
-      <button onClick={AddCounter}>+</button>
-      {'\t'}
-      <span>{counter}</span>
-      {'\t'}
-      <button onClick={SubtractCounter}>-</button>
-    </div>
-  )
+    return (
+        <div>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+        </div>
+    );
 }
 
-export default Counter
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Counter from './App.jsx'
+import Counter from './Counter.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div>
     <Counter />
+    <App />
   </div>
 )
